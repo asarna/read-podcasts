3cd1eb243aa4cc6839fe9be72b784c45d4cdfdab
KON3427 <aubrey.sarna@kroger.com>
2018-04-03 16:17:47 -0400

    unit test additions

--- client/src/components/PodPicker.test.js
+++ client/src/components/PodPicker.test.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import PodPicker from './PodPicker';
+import PodLister from './PodLister';
 import { shallow } from 'enzyme';
 
 describe('PodPicker component', () => {
+  const el = shallow(<PodPicker/>);
+
   it('renders', () => {
-    const el = shallow(<PodPicker/>);
-    
     expect(el.length).toEqual(1);
+  });
+
+  it('should not render PodLister', () => {
+    expect(el.find(PodLister).length).toEqual(0);
   });
 });
