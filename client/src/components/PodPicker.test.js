e8be92b0471ef55a13602d3c40db46146d65cc8e
KON3427 <aubrey.sarna@kroger.com>
2018-06-19 11:50:03 -0400

    update podpicker tests

--- client/src/components/PodPicker.test.js
+++ client/src/components/PodPicker.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import PodPicker from './PodPicker';
 import PodLister from './PodLister';
+import PodItem from './PodItem';
 import { shallow } from 'enzyme';
 import axios from 'axios';
 
@@ -17,25 +18,25 @@ jest.mock('axios', () => {
 
 
 describe('PodPicker component', () => {
-  const el = shallow(<PodPicker/>);
+  let el;
+
+  beforeEach(() => {
+    el = shallow(<PodPicker
+    />);
+  });
 
   it('renders', () => {
     expect(el.length).toEqual(1);
   });
 
-  it('should not render PodLister', () => {
-    expect(el.find(PodLister).length).toEqual(0);
-  });
-
-  it('should show PodLister when search is made', async (done) => {
+  it('should populate PodLister when search is made', async (done) => {
     el.setState({searchTerm: 'test'});
     await el.instance().handleSearch().then(() => {
       el.update();
-      expect(el.find(PodLister).length).toEqual(2);
-      expect(el.find(PodLister).props().items).toEqual([{
+      expect(el.find(PodLister).first().props().items).toEqual([{
         title: 'some podcast'
       }]);
       done();
     }); 
-  })
+  });
 });
